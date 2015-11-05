2c014af553b2539632deb700f6cfb53faa2892d8
nolikaohkawa <s10124no@gmail.com>
2015-11-05 06:12:32 +0000

    add login/out function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #sessions_helper.rbのモジュール読み込み
+  include SessionsHelper
+  
+  private
+  
+  #logged_in?がfalseのとき（ログインしていないとき）に,
+  #アクセスしようとしたURLを保存し、ログイン画面のURLにリダイレクト
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
